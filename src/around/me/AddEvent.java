7470db8dfedbe54c7329bf3f2aa859b3a63b8e77
Rafael Souza <rafael@maisweb.org>
2012-10-24 20:23:20 -0300

    Criando evento e redirecionando pro mapa

--- src/around/me/AddEvent.java
+++ src/around/me/AddEvent.java
@@ -2,7 +2,13 @@ package around.me;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import around.me.models.Event;
 
 public class AddEvent extends Activity {
 
@@ -10,6 +16,20 @@ public class AddEvent extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+        
+        Button buttonCreate = (Button) findViewById(R.id.add_event_criar);
+        
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				EditText name = (EditText) findViewById(R.id.add_event_name);
+				EditText description = (EditText) findViewById(R.id.add_event_description);
+				
+				Event.create(name.getText().toString(), description.getText().toString());
+				
+				Intent intent_map = new Intent(v.getContext(), AroundMe.class);
+	            startActivity(intent_map);
+			}
+		});
     }
 
     @Override
--- src/around/me/models/Event.java
+++ src/around/me/models/Event.java
@@ -1,5 +1,7 @@
 package around.me.models;
 
+import java.util.ArrayList;
+
 public class Event {
 	private int x;
 	private int y;
@@ -7,12 +9,27 @@ public class Event {
 	private String name;
 	private String description;
 	
+	private static ArrayList<Event> events = new ArrayList<Event>();
+
 	private Event(){};
 	
-	public Event(int x, int y, String name, String description){
+	private Event(int x, int y, String name, String description){
 		this.x = x;
 		this.y = y;
 		this.name = name;
 		this.description = description; 
+	}
+	
+	private Event(String name, String description){
+		this.name = name;
+		this.description = description; 
+	}
+	
+	public static void create(String name, String description) {
+		events.add(new Event(name, description));
+	}
+	
+	public static ArrayList<Event> getEvents() {
+		return (ArrayList<Event>) events.clone();
 	}
 }
