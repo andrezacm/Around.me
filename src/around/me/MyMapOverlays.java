1da970dc1b5f7e3f9f4a6643eefdcf9f40403309
Andreza Medeiros <andreza.cmedeiros@gmail.com>
2012-10-25 16:31:52 -0300

    Corrigindo ver detalhes do evento a partir do click no ponto

--- src/around/me/MyMapOverlays.java
+++ src/around/me/MyMapOverlays.java
@@ -6,6 +6,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.MotionEvent;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
@@ -15,26 +18,64 @@ public class MyMapOverlays extends Overlay{
 
 	GeoPoint location;
 	Resources resources;
-	
+
 	public MyMapOverlays(GeoPoint location, Resources resources) {
 		this.location = location;
 		this.resources = resources;
 	}
-	
+
 	@Override
 	public void draw(Canvas canvas, MapView mapview, boolean shadow) {
 		Point screenpoint = new Point();
 		mapview.getProjection().toPixels(location, screenpoint);
+
+		 Bitmap markerImage = BitmapFactory.decodeResource(this.resources, R.drawable.mylocation);
+		 
+		    canvas.drawBitmap(markerImage,
+		        screenpoint.x - markerImage.getWidth() / 2,
+		        screenpoint.y - markerImage.getHeight() / 2, null);
 		
-		canvas.drawBitmap(BitmapFactory.decodeResource(this.resources, R.drawable.mylocation),
-				screenpoint.x, screenpoint.y, null);
-		
+		//canvas.drawBitmap(BitmapFactory.decodeResource(this.resources, R.drawable.mylocation),
+			//	screenpoint.x, screenpoint.y, null);
+
 	}
-	
+
 	@Override
 	public boolean onTouchEvent(android.view.MotionEvent e, MapView mapView){
-		Intent intent_view = new Intent(mapView.getContext(), ViewEvent.class);
-        mapView.getContext().startActivity(intent_view);
-		return true;
+
+		float startX = location.getLatitudeE6();
+		float startY = location.getLongitudeE6();
+		
+		Bitmap markerImage = BitmapFactory.decodeResource(this.resources, R.drawable.mylocation);
+
+		@SuppressWarnings("deprecation")
+		Drawable d = new BitmapDrawable(this.resources, markerImage);
+
+		if (e.getAction() == MotionEvent.ACTION_DOWN) {
+			if (d != null) {
+				int[] states = {android.R.attr.state_pressed};
+				if (d.setState(states)) {
+					d.invalidateSelf();
+				}
+			}
+			startX = e.getX();
+			startY = e.getY();
+			return true;
+		} else if (e.getAction() == MotionEvent.ACTION_UP) {
+			if (d != null) {
+				int newStates[] = {};
+				if (d.setState(newStates)) {
+					d.invalidateSelf();
+				}
+			}
+			if (Math.abs(startX - e.getX()) < 32 && Math.abs(startY - e.getY()) < 49 ) {
+				Intent intent_view = new Intent(mapView.getContext(), ViewEvent.class);
+				mapView.getContext().startActivity(intent_view);
+			}
+			return true;
+
+		} else {
+			return false;
+		}
 	}
 }
